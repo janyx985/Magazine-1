ed54c42441fb1d231a7e7d42485af9f5fd8a2cb6
vf9QaDj <hsepulvedan@gmail.com>
2014-12-04 10:43:44 -0300

    Corregidos ciertos errores

--- app/models/User.php
+++ app/models/User.php
@@ -21,13 +21,64 @@ class User extends Eloquent implements UserInterface, RemindableInterface {
 	 *
 	 * @var array
 	 */
+
+	public function isValid($data)
+    {
+        $rules = array(
+            'email'     => 'required|email|unique:users',
+            'username' => 'required|min:4|max:40',
+            'name' => 'required|min:4|max:40',
+            'password'  => 'min:8|confirmed'
+        );
+        
+        // Si el usuario existe:
+        if ($this->exists)
+        {
+            //Evitamos que la regla “unique” tome en cuenta el email del usuario actual
+			$rules['email'] .= ',email,' . $this->id;
+        }
+        else // Si no existe...
+        {
+            // La clave es obligatoria:
+            $rules['password'] .= '|required';
+        }
+        
+        $validator = Validator::make($data, $rules);
+        
+        if ($validator->passes())
+        {
+            return true;
+        }
+        
+        $this->errors = $validator->errors();
+        
+        return false;
+    }
+	public function validAndSave($data)
+    {
+        // Revisamos si la data es válida
+        if ($this->isValid($data))
+        {
+            // Si la data es valida se la asignamos al usuario
+            $this->fill($data);
+            // Guardamos el usuario
+            $this->save();
+            
+            return true;
+        }
+        
+        return false;
+    }
+
 	protected $hidden = array('password', 'remember_token');
+	protected $fillable = array('email', 'username', 'name', 'password');
 	/*
 	Segun: http://laravel.com/docs/4.2/upgrade#upgrade-4.1.26
 	Además de crear una columna llamada remember_token en la bd,
 	if you are using the Eloquent authentication driver, 
 	update your User class with the following three methods:
 	*/
+
 	public function getRememberToken()
 	{
     	return $this->remember_token;
@@ -42,6 +93,14 @@ class User extends Eloquent implements UserInterface, RemindableInterface {
 	{
     	return 'remember_token';
 	}
+
+	public function setPasswordAttribute($value)
+    {
+        if ( ! empty ($value))
+        {
+            $this->attributes['password'] = Hash::make($value);
+        }
+    }
 
 
 }
